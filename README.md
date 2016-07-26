601469ea04162065f54dc046d64bee1bb82017f2
Ryan Morr <rm.morr@gmail.com>
2016-07-26 15:09:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sonic
+
+> A modern, lightweight, context-aware, and extentable CSS selector engine built on top of `querySelectorAll`.
---
+++ test/test-sonic.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import { jsdom } from 'jsdom';
+import sonic from '../src/sonic';
+
+describe('test suite', () => {
+    it('test case', () => {
+        
+    });
+});
