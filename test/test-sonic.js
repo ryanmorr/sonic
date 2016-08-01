457a6ac833fa405c847dcbde7c657516ea730eee
Ryan Morr <rm.morr@gmail.com>
2016-07-31 23:45:51 -0400

    Add tests for combinators without spaces

--- test/test-sonic.js
+++ test/test-sonic.js
@@ -347,6 +347,7 @@ describe('sonic', () => {
                 checkSelectors([
                     {selector: 'section > div', length: 7},
                     {selector: 'section > div > span[foo]', length: 4},
+                    {selector: 'section>ul>li', length: 5},
                     {selector: 'ul > .list-item', context: group2, length: 5}
                 ]);
             });
@@ -354,6 +355,7 @@ describe('sonic', () => {
             it('should support starting child combinator (>)', () => {
                 checkSelectors([
                     {selector: '> div', context: group1, expected: body.querySelectorAll('#group-1 > div'), length: 4},
+                    {selector: '>div', context: group2, expected: body.querySelectorAll('#group-2 > div'), length: 3},
                     {selector: '> div > i', context: group1, expected: body.querySelectorAll('#group-1 > div > i'), length: 1}
                 ]);
             });
@@ -362,6 +364,7 @@ describe('sonic', () => {
                 checkSelectors([
                     {selector: 'div[foo] + div', length: 1},
                     {selector: 'div + i + em + span', length: 1},
+                    {selector: 'div+span', length: 1},
                     {selector: '.list-item + .list-item', context: group2, length: 4}
                 ]);
             });
@@ -369,7 +372,8 @@ describe('sonic', () => {
             it('should support starting adjacent sibling combinator (+)', () => {
                 checkSelectors([
                     {selector: '+ section', context: group1, expected: body.querySelectorAll('#group-1 + section'), length: 1},
-                    {selector: '+ div', context: element1, expected: body.querySelectorAll('#group-1 > :first-child + div'), length: 1}
+                    {selector: '+ div', context: element1, expected: body.querySelectorAll('#group-1 > :first-child + div'), length: 1},
+                    {selector: '+div', context: element1, expected: body.querySelectorAll('#group-1 > :first-child + div'), length: 1}
                 ]);
             });
 
@@ -377,6 +381,7 @@ describe('sonic', () => {
                 checkSelectors([
                     {selector: 'div[foo] ~ div', context: body, length: 4},
                     {selector: 'div ~ em ~ span', context: body, length: 2},
+                    {selector: 'em~span', context: body, length: 2},
                     {selector: 'h1 ~ [attr2]', context: group2, length: 2}
                 ]);
             });
@@ -384,7 +389,8 @@ describe('sonic', () => {
             it('should support starting general sibling combinator (~)', () => {
                 checkSelectors([
                     {selector: '~ section', context: group1, expected: body.querySelectorAll('#group-1 ~ section'), length: 2},
-                    {selector: '~ div', context: element1, expected: body.querySelectorAll('#group-1 > :first-child ~ div'), length: 3}
+                    {selector: '~ div', context: element1, expected: body.querySelectorAll('#group-1 > :first-child ~ div'), length: 3},
+                    {selector: '~div', context: element1, expected: body.querySelectorAll('#group-1 > :first-child ~ div'), length: 3}
                 ]);
             });
         });
