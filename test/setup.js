ab84f3152a1ba252868dc9c8780866a6208a9120
Ryan Morr <rm.morr@gmail.com>
2019-02-08 16:22:49 -0500

    Improve base level find/query tests

--- test/setup.js
+++ test/setup.js
@@ -1,78 +1,30 @@
-import { matches, find, query, pseudos } from '../src/sonic';
+const elements = [];
 
-const html = `
-    <section id="group-1">
-        <div id="element-1" class="class1 class2 class3" attr="value" attr2="some random text" lang="en-us"></div>
-        <div>
-            <span foo></span>
-            <span></span>
-            <span foo></span>
-            <span foo></span>
-        </div>
-        <div foo>
-            <div foo></div>
-            <i foo></i>
-            <em></em>
-            <span></span>
-            <span foo></span>
-            <div foo></div>
-        </div>
-        <div class="class1 class2" lang="en-us"></div>
-    </section>
-    <section id="group-2">
-        <h1>Top Heading</h1>
-        <section>
-            <h1>Sub Heading</h1>
-        </section>
-        <div foo></div>
-        <ul class="list">
-            <li class="list-item"></li>
-            <li class="list-item"></li>
-            <li class="list-item"></li>
-            <li class="list-item"></li>
-            <li class="list-item"></li>
-        </ul>
-        <div attr="value" attr2="some random text"></div>
-        <span attr2="some random text"></span>
-        <div class="class1 class2"></div>
-    </section>
-    <section id="group-3">
-        <form>
-            <input type="checkbox" checked />
-            <input type="checkbox" checked />
-            <input type="checkbox" />
-            <input type="text" disabled />
-        </form>
-    </section>
-`;
-
-// Populate the DOM with test markup
-document.body.innerHTML = html;
-
-// Export contextual elements
-export const body = document.body;
-export const group1 = document.querySelector('#group-1');
-export const group2 = document.querySelector('#group-2');
-export const group3 = document.querySelector('#group-3');
-export const element1 = document.querySelector('#element-1');
-
-export function checkSelectors(items) {
-    items.forEach((item) => {
-        const context = item.context || document;
-        let expected = item.expected;
-        if (!expected) {
-            expected = context.querySelectorAll(item.selector);
+afterEach(() => {
+    elements.forEach((el) => {
+        if (el.parentNode) {
+            el.parentNode.removeChild(el);
         }
-        const element = find(item.selector, context);
-        const elements = query(item.selector, context);
-        checkElements(expected, element, elements, item.length);
     });
+});
+
+function parseHTML(html) {
+    const frag = document.createDocumentFragment();
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    while (div.firstChild) {
+        frag.appendChild(div.firstChild);
+    }
+    return frag;
 }
 
-function checkElements(expected, element, elements, expectedLength) {
-    expect(elements).to.have.lengthOf(expectedLength);
-    expect(element).to.equal(expected[0] || null);
-    elements.forEach((el, i) => {
-        expect(el).to.equal(expected[i]);
-    });
+export function append(html, expected) {
+    const frag = parseHTML(html);
+    for (const child of frag.childNodes) {
+        elements.push(child);
+    }
+    document.body.appendChild(frag);
+    if (expected) {
+        return document.querySelectorAll(expected);
+    }
 }
--- test/specs/sonic.js
+++ test/specs/sonic.js
@@ -1,43 +1,104 @@
-import { checkSelectors, body, group1, group2, group3, element1 } from '../setup';
-import { matches, find, query, pseudos } from '../../src/sonic';
+import { append } from '../setup';
+import { find, query } from '../../src/sonic';
 
 describe('sonic', () => {
     it('query should return an array', () => {
+        append('<div></div>');
+
         expect(query('div')).to.be.an('array');
     });
 
-    it('find should return null if no element is found', () => {
-        expect(find('#foobar')).to.be.a('null');
+    it('find should return an element', () => {
+        append('<div></div>');
+
+        const div = find('div');
+
+        expect(div.nodeType).to.equal(1);
+        expect(div.nodeName).to.equal('DIV');
     });
 
-    it('should be context-aware', () => {
-        const expected = document.querySelector('#group-2 section h1');
-        const element = find('section h1', group2);
-        expect(element).to.not.be.a('null');
-        expect(element).to.equal(expected);
+    it('find should return null if no element is found', () => {
+        expect(find('#nonexistent')).to.equal(null);
+    });
+
+    it('find should return the first element of query', () => {
+        const expected = append('<div></div><div class="foo"></div><div></div>', '.foo');
+
+        const element = find('.foo');
+        const elements = query('.foo');
+
+        expect(element).to.equal(elements[0]);
+        expect(element).to.equal(expected[0]);
     });
 
     it('should support a contextual element as an optional second argument', () => {
-        const expected = group1.querySelectorAll('div');
-        const elements = query('div', group1);
+        const html = `
+            <div id="foo">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        `;
+
+        const expected = append(html, '#foo span');
+        const root = document.querySelector('#foo');
+
+        const elements = query('span', root);
+
+        expect(elements.length).to.equal(expected.length);
         elements.forEach((el, i) => {
-            expect(group1.contains(el)).to.equal(true);
+            expect(el.nodeName).to.equal('SPAN');
+            expect(root.contains(el)).to.equal(true);
             expect(el).to.equal(expected[i]);
         });
     });
 
     it('should support a selector string for a contextual element as an optional second argument', () => {
-        const expected = group1.querySelectorAll('div');
-        const elements = query('div', '#group-1');
+        const html = `
+            <div id="foo">
+                <em></em>
+                <em></em>
+                <em></em>
+            </div>
+        `;
+
+        const expected = append(html, '#foo em');
+        const root = document.querySelector('#foo');
+
+        const elements = query('em', '#foo');
+
+        expect(elements.length).to.equal(expected.length);
         elements.forEach((el, i) => {
-            expect(group1.contains(el)).to.equal(true);
+            expect(el.nodeName).to.equal('EM');
+            expect(root.contains(el)).to.equal(true);
             expect(el).to.equal(expected[i]);
         });
     });
 
+    it('should be context-aware', () => {
+        const html = `
+            <section id="foo">
+                <h1>Top Heading</h1>
+                <section>
+                    <h1>Sub Heading</h1>
+                </section>
+            </section>
+        `;
+
+        const expected = append(html, '#foo section h1');
+        const root = document.querySelector('#foo');
+
+        const element = find('section h1', root);
+
+        expect(element).to.equal(expected[0]);
+    });
+
     it('should not return duplicate elements', () => {
-        const cache = [];
+        append('<div></div><div></div>');
+
         const elements = query('div, div');
+
+        const cache = [];
         elements.forEach((el) => {
             expect(cache).to.not.include(el);
             cache.push(el);
@@ -45,19 +106,20 @@ describe('sonic', () => {
     });
 
     it('should return elements in the order they appear in the document', () => {
+        append('<div></div><div></div>');
+
         const elements = query('div');
+
         const expected = elements.slice().sort((a, b) => 3 - (a.compareDocumentPosition(b) & 6));
-        elements.forEach((el, i) => {
-            expect(el).to.equal(expected[i]);
-        });
+
+        elements.forEach((el, i) => expect(el).to.equal(expected[i]));
     });
 
     it('should accept selector strings with leading/trailing spaces', () => {
-        checkSelectors([
-            {selector: ' div', length: 9},
-            {selector: 'div ', length: 9},
-            {selector: ' div ', length: 9},
-            {selector: '  div   ', length: 9}
-        ]);
+        const expected = append('<div id="foo"></div>', '#foo')[0];
+
+        [' #foo', '#foo ', ' #foo ', '  #foo   '].forEach((selector) => {
+            expect(find(selector)).to.equal(expected);
+        });
     });
 });
